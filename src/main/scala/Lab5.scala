c4d7c4ad9619aa1c61e6678959c421ba07ceaade
Olena Velychko <78988714+olenavelychko-ua@users.noreply.github.com>
2021-06-01 22:55:44 +0300

    New version

--- src/main/scala/Lab5.scala
+++ src/main/scala/Lab5.scala
@@ -1,19 +1,24 @@
-import scala.language.implicitConversions
+import scala.language.{implicitConversions, postfixOps}
 
-trait CommutativeMonoid[A]{
-  def summ(a1:A, a2:A): A
+trait CommutativeMonoid[A] {
   def zero: A
+  def summ(a: A, b: A): A
 }
-object CommutativeMonoid{
+
+object CommutativeMonoid {
   implicit val mapCommutativeMonoid = new CommutativeMonoid[Map[String, Double]] {
-    override def summ(x: Map[String, Double], y: Map[String, Double]):
-    Map[String, Double] = x ++ y
-    override val zero : Map[String, Double] = Map()
+    override def zero: Map[String, Double] = Map()
+
+    override def summ(a: Map[String, Double], b: Map[String, Double]): Map[String, Double] =
+      (a.keySet ++ b.keySet) map { k =>
+        (k, a.getOrElse(k, 0.0) + b.getOrElse(k, 0.0))
+      } toMap
   }
 }
 class CommutativeMonoidOps[A](x: A)(implicit g: CommutativeMonoid[A]) {
   def ++(y: A): A = g.summ(x, y)
 }
+
 object CommutativeMonoidOps {
   def main(args: Array[String]): Unit = {
     implicit def common[A](x: A)(implicit g: CommutativeMonoid[A]): CommutativeMonoidOps[A] = {
--- src/test/scala/CommutativeMonoidSpecification.scala
+++ src/test/scala/CommutativeMonoidSpecification.scala
@@ -1,26 +1,43 @@
 import org.scalacheck.Properties
 import org.scalacheck.Prop.forAll
-import CommutativeMonoidOps._
+
 
 object MonoidSpecification extends Properties("CommutativeMonoid") {
-  property("associativity") = forAll {
+  property("associativity Summ") = forAll {
     (a: Map[String, Double], b: Map[String, Double], c: Map[String, Double]) =>
-      var s1 : Map[String, Double] = (a ++ b) ++ c
-      var s2 : Map[String, Double] = a ++ (b ++ c)
-      s1 == s2
+      var s1: Map[String, Double] = CommutativeMonoid.mapCommutativeMonoid.summ(a, CommutativeMonoid.mapCommutativeMonoid.summ(b, c))
+      var s2: Map[String, Double] = CommutativeMonoid.mapCommutativeMonoid.summ(b, CommutativeMonoid.mapCommutativeMonoid.summ(a, c))
+      s1.equals(s2)
   }
-  property("commutability") = forAll {
+  property("associativity FoldLeft") = forAll {
+    (a: Map[String, Double], b: Map[String, Double], c: Map[String, Double]) =>
+
+      val list1 = List(a, b, c)
+      val s1 = list1.foldLeft(CommutativeMonoid.mapCommutativeMonoid.zero)(CommutativeMonoid.mapCommutativeMonoid.summ)
+      val list2 = List(b, a, c)
+      var s2 = list2.foldLeft(CommutativeMonoid.mapCommutativeMonoid.zero)(CommutativeMonoid.mapCommutativeMonoid.summ)
+      s1.equals(s2)
+  }
+  property("commutability summ") = forAll {
     (a: Map[String, Double], b: Map[String, Double]) =>
-      var s1 : Map[String, Double] = a ++ b
-      var s2 : Map[String, Double] = b ++ a
-      s1 == s2
+      var s1: Map[String, Double] = CommutativeMonoid.mapCommutativeMonoid.summ(a, b)
+      var s2: Map[String, Double] = CommutativeMonoid.mapCommutativeMonoid.summ(b, a)
+      s1.equals(s2)
+  }
+  property("commutability FoldLeft") = forAll {
+    (a: Map[String, Double], b: Map[String, Double]) =>
+      val list1 = List(a, b)
+      val s1 = list1.foldLeft(CommutativeMonoid.mapCommutativeMonoid.zero)(CommutativeMonoid.mapCommutativeMonoid.summ)
+      val list2 = List(b, a)
+      var s2 = list2.foldLeft(CommutativeMonoid.mapCommutativeMonoid.zero)(CommutativeMonoid.mapCommutativeMonoid.summ)
+      s1.equals(s2)
   }
   property("neutral") = forAll {
     (a: Map[String, Double]) =>
-      val z : Map[String, Double] = CommutativeMonoid.mapCommutativeMonoid.zero
-      var s1 : Map[String, Double] = a ++ z
-      var s2 : Map[String, Double] = z ++ a
-      s1 == a
-      s2 == a
+      val z: Map[String, Double] = CommutativeMonoid.mapCommutativeMonoid.zero
+      var s1: Map[String, Double] = CommutativeMonoid.mapCommutativeMonoid.summ(a, z)
+      var s2: Map[String, Double] = CommutativeMonoid.mapCommutativeMonoid.summ(z, a)
+      s1.equals(a)
+      s2.equals(a)
   }
 }
